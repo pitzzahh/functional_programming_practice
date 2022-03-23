a7648cda3e0f92f377ab60d4a91cffea11508b19
pitzzahh <97332554+pitzzahh@users.noreply.github.com>
2022-03-23 20:57:32 +0800

    converted Person class to a record

--- src/main/java/imperative/Main.java
+++ src/main/java/imperative/Main.java
@@ -63,15 +63,8 @@ public class Main {
                 .toList()
                 .forEach(System.out::println);
     }
-    static class Person {
-        private final String name;
-        private final Gender gender;
 
-        Person(String name, Gender gender) {
-            this.name = name;
-            this.gender = gender;
-        }
-
+    record Person(String name, Gender gender) {
         @Override
         public String toString() {
             return "Person{" +
