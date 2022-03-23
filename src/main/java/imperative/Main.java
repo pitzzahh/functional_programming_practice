f7222c516885b2de55e271a28c3e04d4b8d67525
pitzzahh <97332554+pitzzahh@users.noreply.github.com>
2022-03-23 21:01:07 +0800

    reverted Person class to a normal class

--- src/main/java/imperative/Main.java
+++ src/main/java/imperative/Main.java
@@ -63,8 +63,15 @@ public class Main {
                 .toList()
                 .forEach(System.out::println);
     }
+    static final class Person {
+        private final String name;
+        private final Gender gender;
 
-    record Person(String name, Gender gender) {
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
         @Override
         public String toString() {
             return "Person{" +
